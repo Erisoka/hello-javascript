1d920848b4ae39d249e2f4d3a7e66e0d7bad7b34
Erisoka <leonidas.asprilla@outlook.com>
2025-12-29 20:57:12 -0500

    Hola Mundo y Operadores Aritmeticos Basicos

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,17 @@
+// Esto es un comentario en JavaScript
+
+/*
+Esto es un comentario
+de múltiples líneas
+*/
+
+console.log("Hola, JavaScript!")
+console.log('Hola, JavaScript!')
+console.log(`Hola, JavaScript!`)  
+
+console.log("5 + 3 =", 5 + 3)  // Suma
+console.log("10 - 2 =", 10 - 2) // Resta
+console.log("4 * 2 =", 4 * 2)   // Multiplicación
+console.log("8 / 2 =", 8 / 2)   // División
+console.log("7 % 3 =", 7 % 3)   // Módulo
+console.log("2 ** 3 =", 2 ** 3) // Exponenciación
