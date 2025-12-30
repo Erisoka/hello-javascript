68cfa2cb6b02f3603cfef57d9f3ac02e7fb5e772
Erisoka <leonidas.asprilla@outlook.com>
2025-12-29 21:05:42 -0500

    Variables

---
+++ Basic/01-variables.js
@@ -0,0 +1,30 @@
+// var
+
+var helloWorld = "Hello, World!"
+console.log(helloWorld)
+
+helloWorld = "Hello, JavaScript!"
+console.log(helloWorld)
+
+// let (block-scoped variable)
+
+let greeting = "Hi there!"
+console.log(greeting)
+
+greeting = "Hello again!"
+console.log(greeting)
+
+// const
+
+const pi = 3.14159
+console.log("Value of pi:", pi)
+
+// pi = 3.14 // This will cause an error because 'pi' is a constant
+
+// Variable naming conventions
+let myVariable = 10
+let _myVariable = 20
+let $myVariable = 30
+// let 1stVariable = 40 // This will cause an error because variable names cannot start with a number
+let camelCaseVariable = 50
+console.log("Variables:", myVariable, _myVariable, $myVariable, camelCaseVariable)
